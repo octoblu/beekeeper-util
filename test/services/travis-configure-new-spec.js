0eaf74847be45f34c43679c7f6eceb07f831c897
Peter DeMartini <thepeterdemartini@gmail.com>
2017-09-11 14:12:57 -0700

    getting some nice mocks
    Signed-off-by: Jade Meskill <jade.meskill@gmail.com>

---
+++ test/services/travis-configure-new-spec.js
@@ -0,0 +1,118 @@
+const { beforeEach, afterEach, describe, it } = global
+const { expect } = require("chai")
+const TravisService = require("../../lib/services/travis-service")
+const TravisMocks = require("./travis-mocks")
+
+describe("Configure a new travis-ci project", function() {
+  beforeEach("create service", function() {
+    this.sut = new TravisService({
+      github: {
+        token: "github-token",
+      },
+      travis: {
+        env: [
+          {
+            value: "some-value",
+            name: "SOME_ENV_NAME",
+          },
+        ],
+      },
+    })
+  })
+
+  describe("when the repo is public", function() {
+    beforeEach("setup travis mocks", function() {
+      this.travisMocks = new TravisMocks({
+        githubToken: "github-token",
+        isPrivate: false,
+      })
+    })
+
+    afterEach("clean up travis mocks", function() {
+      this.travisMocks.cleanup()
+    })
+
+    beforeEach("setup travis endpoints", function() {
+      const envVars = [{ name: "SOME_ENV_NAME", value: "some-value" }]
+      this.travisMocks
+        .getRepoWithSync()
+        .enableRepo()
+        .getEnvVars([])
+        .createEnvVars(envVars)
+    })
+
+    beforeEach("call configure", function() {
+      const options = {
+        projectName: "example-repo-name",
+        projectOwner: "some-owner",
+        isPrivate: false,
+      }
+      return this.sut.configure(options)
+    })
+
+    it("should call of the travis endpoints", function() {
+      this.travisMocks.done()
+    })
+  })
+
+  describe("when the repo is private", function() {
+    beforeEach("setup travis mocks", function() {
+      this.travisMocks = new TravisMocks({
+        githubToken: "github-token",
+        isPrivate: true,
+      })
+    })
+
+    afterEach("clean up travis mocks", function() {
+      this.travisMocks.cleanup()
+    })
+
+    beforeEach("setup travis endpoint", function() {
+      const envVars = [{ name: "SOME_ENV_NAME", value: "some-value" }]
+      this.travisMocks
+        .getRepoWithSync()
+        .enableRepo()
+        .getEnvVars([])
+        .createEnvVars(envVars)
+    })
+
+    beforeEach("call configure", function() {
+      const options = {
+        projectName: "example-repo-name",
+        projectOwner: "some-owner",
+        isPrivate: true,
+      }
+      return this.sut.configure(options)
+    })
+
+    it("should call of the travis endpoints", function() {
+      this.travisMocks.done()
+    })
+  })
+
+  describe("when travis responds with a 403", function() {
+    beforeEach("setup travis mocks", function() {
+      this.travisMocks = new TravisMocks({
+        githubToken: "github-token",
+        isPrivate: false,
+      })
+    })
+
+    afterEach("clean up travis mocks", function() {
+      this.travisMocks.cleanup()
+    })
+
+    beforeEach("setup travis endpoints", function() {
+      this.travisMocks.authed.get("/repos/some-owner/example-repo-name").reply(403)
+    })
+
+    it("should be rejected", function() {
+      const options = {
+        projectName: "example-repo-name",
+        projectOwner: "some-owner",
+        isPrivate: false,
+      }
+      expect(this.sut.configure(options)).to.be.rejected
+    })
+  })
+})
---
+++ test/services/travis-mocks.js
@@ -0,0 +1,101 @@
+const nock = require("nock")
+const uuid = require("uuid")
+const bindAll = require("lodash/fp/bindAll")
+const each = require("lodash/each")
+
+class TravisMocks {
+  constructor({ githubToken, isPrivate }) {
+    bindAll(Object.getOwnPropertyNames(TravisMocks.prototype), this)
+    const uri = isPrivate ? "https://api.travis-ci.com" : "https://api.travis-ci.org"
+    const accessToken = uuid.v1()
+
+    this._tokenExchange = nock(uri)
+      .matchHeader("accept", "application/json")
+      .matchHeader("user-agent", "Travis CI/1.0")
+      .post("/auth/github", { github_token: githubToken })
+      .reply(200, { access_token: accessToken })
+
+    this.authed = nock(uri)
+      .matchHeader("accept", "application/json")
+      .matchHeader("user-agent", "Travis CI/1.0")
+      .matchHeader("authorization", `token ${accessToken}`)
+  }
+
+  getRepo() {
+    this.authed.get("/repos/some-owner/example-repo-name").reply(200, {
+      id: "travis-repo-id",
+    })
+    return this
+  }
+
+  getRepoWithSync() {
+    this.authed
+      .get("/repos/some-owner/example-repo-name")
+      .reply(404)
+      .post("/users/sync")
+      .reply(200)
+      .get("/repos/some-owner/example-repo-name")
+      .reply(200, {
+        id: "travis-repo-id",
+      })
+    return this
+  }
+
+  enableRepo() {
+    this.authed
+      .put("/hooks", {
+        hook: {
+          id: "travis-repo-id",
+          active: true,
+        },
+      })
+      .reply(204)
+    return this
+  }
+
+  getEnvVars(envVars) {
+    this.authed.get("/settings/env_vars?repository_id=travis-repo-id").reply(200, {
+      env_vars: envVars,
+    })
+    return this
+  }
+
+  updateEnvVars(envVars) {
+    each(envVars, ({ name, value, id }) => {
+      this.authed
+        .patch(`/settings/env_vars/${id}?repository_id=travis-repo-id`, {
+          repository_id: "travis-repo-id",
+          name: name,
+          value: value,
+          public: false,
+        })
+        .reply(204)
+    })
+    return this
+  }
+
+  createEnvVars(envVars) {
+    each(envVars, ({ name, value }) => {
+      this.authed
+        .post("/settings/env_vars?repository_id=travis-repo-id", {
+          repository_id: "travis-repo-id",
+          name: name,
+          value: value,
+          public: false,
+        })
+        .reply(204)
+    })
+    return this
+  }
+
+  cleanup() {
+    nock.cleanAll()
+  }
+
+  done() {
+    this._tokenExchange.done()
+    this.authed.done()
+  }
+}
+
+module.exports = TravisMocks
