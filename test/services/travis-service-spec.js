12ec40b174fe312885888ecf64d70d6bb58849b4
Jade Meskill <jade.meskill@gmail.com>
2017-09-11 11:58:02 -0700

    travis sync and promises
    Signed-off-by: Peter DeMartini <thepeterdemartini@gmail.com>

--- test/services/travis-service-spec.js
+++ test/services/travis-service-spec.js
@@ -16,21 +16,36 @@ describe("TravisService", function() {
     nock.cleanAll()
   })
 
+  beforeEach("setup travis mocks", function() {
+    this.travisOrg = nock("https://api.travis-ci.org")
+      .matchHeader("accept", "application/json")
+      .matchHeader("user-agent", "Travis CI/1.0")
+      .post("/auth/github", { github_token: "github-token" })
+      .reply(200, { access_token: "foobar" })
+    this.travisCom = nock("https://api.travis-ci.com")
+      .matchHeader("accept", "application/json")
+      .matchHeader("user-agent", "Travis CI/1.0")
+      .post("/auth/github", { github_token: "github-token" })
+      .reply(200, { access_token: "foobar" })
+    this.travisOrgAuthed = nock("https://api.travis-ci.org")
+      .matchHeader("accept", "application/json")
+      .matchHeader("user-agent", "Travis CI/1.0")
+      .matchHeader("authorization", "token foobar")
+    this.travisComAuthed = nock("https://api.travis-ci.com")
+      .matchHeader("accept", "application/json")
+      .matchHeader("user-agent", "Travis CI/1.0")
+      .matchHeader("authorization", "token foobar")
+  })
+
   describe("->configure", function() {
     describe("when travis have never been setup", function() {
-      beforeEach("call configure", function(done) {
-        const options = {
-          projectName: "example-repo-name",
-          projectOwner: "some-owner",
-          isPrivate: false,
-        }
-        this.travis = nock("https://api.travis-ci.org")
-          .matchHeader("accept", "application/json")
-          .matchHeader("user-agent", "Travis CI/1.0")
-          .post("/auth/github", { github_token: "github-token" })
-          .reply(200, { access_token: "foobar" })
+      beforeEach("setup travis endpoints", function() {
+        this.travisOrgAuthed
           .get("/repos/some-owner/example-repo-name")
-          .matchHeader("authorization", "token foobar")
+          .reply(404)
+          .post("/users/sync")
+          .reply(200)
+          .get("/repos/some-owner/example-repo-name")
           .reply(200, {
             id: "travis-repo-id",
           })
@@ -40,13 +55,67 @@ describe("TravisService", function() {
               active: true,
             },
           })
-          .matchHeader("authorization", "token foobar")
           .reply(204)
-        this.sut.configure(options, done)
+      })
+
+      beforeEach("call configure", function() {
+        const options = {
+          projectName: "example-repo-name",
+          projectOwner: "some-owner",
+          isPrivate: false,
+        }
+        return this.sut.configure(options)
       })
 
       it("should call of the travis endpoints", function() {
-        this.travis.done()
+        this.travisOrg.done()
+        this.travisOrgAuthed.done()
+      })
+    })
+
+    describe("when the repo is private", function() {
+      beforeEach("setup travis endpoint", function() {
+        this.travisComAuthed
+          .get("/repos/some-owner/example-repo-name")
+          .reply(200, {
+            id: "travis-repo-id",
+          })
+          .put("/hooks", {
+            hook: {
+              id: "travis-repo-id",
+              active: true,
+            },
+          })
+          .reply(204)
+      })
+
+      beforeEach("call configure", function() {
+        const options = {
+          projectName: "example-repo-name",
+          projectOwner: "some-owner",
+          isPrivate: true,
+        }
+        return this.sut.configure(options)
+      })
+
+      it("should call of the travis endpoints", function() {
+        this.travisCom.done()
+        this.travisComAuthed.done()
+      })
+    })
+
+    describe("when travis is ratelimited", function() {
+      beforeEach("setup travis endpoints", function() {
+        this.travisOrgAuthed.get("/repos/some-owner/example-repo-name").reply(403)
+      })
+
+      it("should be rejected", function() {
+        const options = {
+          projectName: "example-repo-name",
+          projectOwner: "some-owner",
+          isPrivate: false,
+        }
+        expect(this.sut.configure(options)).to.be.rejected
       })
     })
   })
--- test/test_helper.coffee
+++ test/test_helper.coffee
@@ -1,8 +1,10 @@
 chai      = require 'chai'
 sinon     = require 'sinon'
 sinonChai = require 'sinon-chai'
+chaiAsPromised = require 'chai-as-promised'
 
 chai.use sinonChai
+chai.use chaiAsPromised
 
 global.expect = chai.expect
 global.sinon  = sinon
