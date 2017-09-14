c1de9bce311cf1074eb2928a36e8f5e145896f5e
Peter DeMartini <thepeterdemartini@gmail.com>
2017-09-14 14:53:33 -0700

    added more status service support

--- test/services/status-service/get-status-service-spec.js
+++ test/services/status-service/get-status-service-spec.js
@@ -1,6 +1,7 @@
 const { beforeEach, afterEach, describe, expect, it } = global
 const StatusService = require("../../../lib/services/status-service")
 const BeekeeperMocks = require("../beekeeper/beekeeper-mocks")
+const ProjectMocks = require("./project-mocks")
 
 describe("Status: get the status of a service", function() {
   describe(`when the status is pending`, function() {
@@ -8,6 +9,7 @@ describe("Status: get the status of a service", function() {
       this.sut = new StatusService({
         beekeeperUri: "https://beekeeper.octoblu.com",
         beekeeperEnabled: true,
+        projectUri: "https://some-repo.example.com",
       })
     })
 
@@ -15,12 +17,27 @@ describe("Status: get the status of a service", function() {
       this.beekeeperMocks = new BeekeeperMocks({ isPrivate: false })
     })
 
+    beforeEach("setup project mocks", function() {
+      this.projectMocks = new ProjectMocks()
+    })
+
     afterEach("clean up beekeeper mocks", function() {
       this.beekeeperMocks.cleanup()
     })
 
+    afterEach("clean up project mocks", function() {
+      this.projectMocks.cleanup()
+    })
+
     beforeEach("setup beekeeper endpoints", function() {
-      this.beekeeperMocks.getDeployment("v1.0.0")
+      this.beekeeperMocks
+        .getDeployment("v1.0.0")
+        .getDeployment("latest")
+        .getDeployment("v2.0.0")
+    })
+
+    beforeEach("setup project endpoints", function() {
+      this.projectMocks.getVersion("v2.0.0")
     })
 
     beforeEach("call get", async function() {
@@ -36,10 +53,20 @@ describe("Status: get the status of a service", function() {
       this.beekeeperMocks.done()
     })
 
+    it("should call of the project endpoints", function() {
+      this.projectMocks.done()
+    })
+
     it("should give respond with the correct result", function() {
       expect(this.result).to.deep.equal({
         desired: {
           tag: "v1.0.0",
+        },
+        latest: {
+          tag: "latest",
+        },
+        running: {
+          tag: "v2.0.0",
         },
       })
     })
